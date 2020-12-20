8cdfe7b49aec029ebd3c4298dc4faf0578d60bfb
HappyBreaker <75553500+HappyBreaker@users.noreply.github.com>
2020-12-20 17:13:42 +0800

    1220

--- 2Dgame/Assets/Script/Car.cs
+++ 2Dgame/Assets/Script/Car.cs
@@ -1,18 +1,52 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /*標題[Header]、提示[Tooltip]、範圍[Range]*/
+    [Header("這是高度")]
+    [Tooltip("這是高度")]
+    [Range( 1, 10)]
+    public int heigher = 0;
+    [Header("這是寬度")]
+    [Tooltip("這是寬度")]
+    [Range( 2, 20)]
+    public float weight = 0;
+    [Header("這是品牌")]
+    [Tooltip("這是品牌")]
+    public string Brand;
+    [Header("這是功能")]
+    [Tooltip("這是功能")]
+    public bool Funtion = true;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //顏色
+    public Color test1;
+    public Color test2 = Color.black;
+    //                              R     G     B
+    public Color test3 = new Color(0.5f, 0.5f, 0.7f);
+    //                                               透明度
+    public Color test4 = new Color(0.4f, 0.2f, 0.4f, 0.5f);
+
+    //維度向量
+    public Vector2 V2Zero = Vector2.zero;
+    public Vector2 V2One = Vector2.one;
+
+    public GameObject OBjTest;
+    public Vector3 v3Test = new Vector3(1, 1, 1);
+    public Vector4 V4test = new Vector4(2, 2, 2, 2);
+
+    //圖片 與 聲音
+    public Sprite picture;
+    public AudioClip Sound;
+
+    //遊戲物件 與 元件
+    public GameObject Obj1;
+    public GameObject Obj2;
+
+    public Transform tranform;
+    public Camera cara;
+
+
+
+
+
 }
---
+++ 2Dgame/Assets/Script/practice.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class practice : MonoBehaviour
+{
+    [Header("移動數值")]
+    [Range(0f,1000f)]
+    public float MoveSpead = 10.5f;
+    [Header("跳越高度")]
+    [Range(0f, 3000f)]
+    public float JumpHeight = 100;
+    [Header("是否在地上")]
+    [Tooltip("是否在地上")]
+    public bool InTheGround = false;
+    [Header("子彈-物件")]
+    [Tooltip("子彈-物件")]
+    public GameObject Bullet;
+    [Header("子彈生成點")]
+    [Tooltip("子彈生成點")]
+    public Transform BulletPosition;
+    [Header("子彈速度")]
+    [Tooltip("子彈速度")]
+    [Range(0,5000)]
+    public int BulletSpead = 800;
+    [Header("開槍音效")]
+    [Tooltip("開槍音效")]
+    public AudioClip Sound;
+    [Header("血量")]
+    [Tooltip("血量")]
+    [Range(0,200)]
+    public int Health = 100;
+
+    private AudioClip Audio;
+    private Rigidbody2D Rigidbody2D;
+    private Animation Animation;
+
+
+
+
+}
