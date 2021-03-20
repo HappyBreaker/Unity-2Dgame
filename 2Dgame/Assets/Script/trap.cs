9fde5657a0b8e0ba7fd7b88290da2a0a67bc8409
HappyBreaker <75553500+HappyBreaker@users.noreply.github.com>
2021-03-21 03:01:00 +0800

    0321

---
+++ 2Dgame/Assets/Script/trap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trap : MonoBehaviour
+{
+    [Header("傳送點一")]
+    public Transform trap1;
+    [Header("陷阱傷害")]
+    public float damege = 10;
+
+    private Player player;
+    private Transform warrior;
+
+    private void Start()
+    {
+        player = FindObjectOfType<Player>();
+        warrior = GameObject.Find("Warrior").transform;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        player.health(damege);
+        warrior.position = trap1.position;
+    }
+
+}
